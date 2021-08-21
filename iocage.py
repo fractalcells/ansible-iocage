402d5d7148cd46bfffbda3c81561b75b194cbb7f
Vladimir Botka <vbotka@gmail.com>
2021-08-21 21:45:08 +0200

    Fix indentation. (Not related to #30)

--- iocage.py
+++ iocage.py
@@ -604,7 +604,7 @@ def jail_create(module, iocage_path, name=None, properties=None, clone_from_name
             cmd = f"{iocage_path} create -T -n {name} -r {release} {_props_to_str(properties)}"
 
         else:
-           cmd = f"{iocage_path} create -n {name} -r {release} {_props_to_str(properties)}"
+            cmd = f"{iocage_path} create -n {name} -r {release} {_props_to_str(properties)}"
 
         if pkglist:
             cmd += " --pkglist=" + pkglist
